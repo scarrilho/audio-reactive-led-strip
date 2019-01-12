442d26df1e3a06c74fe8c805bc4dc243249a5557
scarrilho <seergio.carrilho@gmail.com>
2019-01-12 23:18:31 +0900

    Fix crash caused by overflow on pyaudio
    Based on https://github.com/scottlawsonbc/audio-reactive-led-strip/pull/73/commits/0d0c4d6a3cfa3bec37905d8cd3bd7bdff1ea48e6

--- python/microphone.py
+++ python/microphone.py
@@ -24,6 +24,7 @@ def start_stream(callback):
             #y = np.fromstring(stream.read(frames_per_buffer), dtype=np.int16)
             y = np.fromstring(stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
             y = y.astype(np.float32)
+            stream.read(get_read_available(), exception_on_overflow=False)
             callback(y)
         except IOError:
             overflows += 1
