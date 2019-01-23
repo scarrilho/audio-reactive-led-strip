c4c723d2781854b188611ca23725840b2693e1bf
scarrilho <seergio.carrilho@gmail.com>
2019-01-23 22:20:33 +0900

    Changed number of LEDs to 112
    My setup has 112 LEDs

--- arduino/ws2812_controller/ws2812_controller.ino
+++ arduino/ws2812_controller/ws2812_controller.ino
@@ -6,7 +6,7 @@
 #include <NeoPixelBus.h>
 
 // Set to the number of LEDs in your LED strip
-#define NUM_LEDS 60
+#define NUM_LEDS 112
 // Maximum number of packets to hold in the buffer. Don't change this.
 #define BUFFER_LEN 1024
 // Toggles FPS output (1 = print FPS over serial, 0 = disable output)
