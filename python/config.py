28b279ef7b47611953929cdb89195ee07bd1c261
scarrilho <seergio.carrilho@gmail.com>
2019-01-23 22:19:35 +0900

    Changed number of LEDs to 112
    My setup has 112 LEDs

--- python/config.py
+++ python/config.py
@@ -49,7 +49,7 @@ USE_GUI = True
 DISPLAY_FPS = False
 """Whether to display the FPS when running (can reduce performance)"""
 
-N_PIXELS = 120
+N_PIXELS = 112
 """Number of pixels in the LED strip (must match ESP8266 firmware)"""
 
 GAMMA_TABLE_PATH = os.path.join(os.path.dirname(__file__), 'gamma_table.npy')
